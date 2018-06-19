1f2bcb12ceb3be515cfcfaed5de9db1ab945033d
monsooness <monsooness>
2018-06-19 15:48:04 -0700

    interact

--- src/init.js
+++ src/init.js
@@ -7,23 +7,45 @@ $(document).ready(function() {
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
+      
+      $("body").height() * Math.random(200),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+      // debugger;
     window.dancers.push(dancer);
+    window.dancers[0].$node.addClass( "mouseover" );
+    
+    
+    $('.mouseover').mouseover(function() {
+      $('.mouseover').fadeToggle();
+    })
+
   });
   
   $('.Lineup').click(function() {
     for (let i = 0; i < window.dancers.length; i++) {
-      window.dancers[i].$node.animate({left : '250px'});
+      window.dancers[i].$node.animate({top : '450px'});
     }
   });
-  $(window.dancers[0].$node).mouseover(function() {
-    $(window.dancers.$node).hide();
-  })
   
+  $('.interact').click(function() {
+    
+    for (var i = 0; i < window.dancers.length; i++) {
+      // debugger;
+      if (Math.abs( window.dancers[i].$node[0].clientHeight - window.dancers[i+1].$node[0].clientHeight ) < 50
+      && Math.abs( window.dancers[i].$node[0].clientWidth - window.dancers[i+1].$node[0].clientWidth ) < 50
+    ) {
+        window.dancers[0].$node.animate({left : '250px'});
+        window.dancers[1].$node.animate({left : '250px'});
+        window.dancers[3].$node.animate({right : '250px'});
+        window.dancers[4].$node.animate({right : '250px'});
+      }
+    }
+    console.log('clicked');
+  })
+ 
 
 });
 
