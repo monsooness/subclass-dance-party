99753aeee1fd6402a4d071d75fa3b3cef31bf196
monsooness <monsooness>
2018-06-19 11:55:41 -0700

    Added gifs

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,22 +1,6 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  // debugger;
-  var blinky = new makeDancer(top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  blinky.step = function() {
-    this.$node.toggle();
-  };
-  
-  setTimeout(function() {
-    setInterval(function() {
-    blinky.step()
-  }, timeBetweenSteps);
-  }, timeBetweenSteps)
-
-  
-  return blinky
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.prepend('<img src="https://media.giphy.com/media/X27NJNfskDfJm/source.gif" alt="Italian Trulli"/>')
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype)
@@ -24,5 +8,6 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  this.$node.toggle();
+  this.$node.animate({top : '800px'});
+  
 }
--- src/sizedDancer.js
+++ src/sizedDancer.js
@@ -1,26 +1,15 @@
 // sizedDancer.js
 var makeSizedDancer = function(top, left, timeBetweenSteps) {
-  // debugger;
-  var sized = new makeDancer(top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-
-  sized.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    // var styleSettings = {
-    //   $('.add3DButton').click(function() {
-    // $(this).css('border-radius' : "20px");
-    // }); 
-  sized.$node.fadeToggle()
-  };
-  setInterval(function() {
-    sized.step()
-  }, timeBetweenSteps);
-  
-  return sized
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node.prepend('<img src="https://media.giphy.com/media/l3q2U7LzaWWYebQf6/source.gif" alt="Italian Trulli"/>')
 };
+
+makeSizedDancer.prototype = Object.create(makeDancer.prototype)
+makeSizedDancer.prototype.constructor = makeSizedDancer;
+
+makeSizedDancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+  this.$node.fadeToggle();
+  this.$node.animate({top : '850px'});
+   this.$node.css({color:'black'});
+}
