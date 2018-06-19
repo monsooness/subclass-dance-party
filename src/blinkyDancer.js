01eeb23bbc8ddf84b051a450ebff866309abe47b
monsooness <monsooness>
2018-06-19 12:07:46 -0700

    modified dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,6 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-  this.$node.prepend('<img src="https://media.giphy.com/media/X27NJNfskDfJm/source.gif" alt="Italian Trulli"/>')
+  this.$node.prepend('<img src="https://media.giphy.com/media/X27NJNfskDfJm/source.gif" alt="Italian Trulli" height="22" width="22"/>')
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype)
@@ -8,6 +8,6 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  this.$node.animate({top : '800px'});
+  this.$node.css({top : '100px'});
   
 }
